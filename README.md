b2ac5d7ee84efa4fa20b7aed719fb4773fb8ca2b
Ed Reynolds <edwreynolds@github.com>
2019-02-23 12:16:54 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Prototype app using Spring Security OAuth clinet to force login to page 
+localhost:8099/secured/home but not require authentication to reach 
+default page localhost:8099
+
+Uses: Spring Boot(2.13), WebMVC, Security, OAuth Client Lombok, Java 8
+ 
+Github is used for OAuth authentication
---
+++ src/main/java/com/epr/SecurityOauth1Application.java
@@ -0,0 +1,26 @@
+package com.epr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Prototype app using Spring Security and OAuth client to force login to 
+ * page localhost:8099/secured/home but not require authentication to 
+ * reach default page localhost:8099
+ * 
+ * Use: Spring Boot(2.13), WebMVC, Security, OAuth Client, Lombok, Java 8
+ * 
+ * Github is being used for OAuth authentication
+ * 
+ * Banner courtesy of: http://patorjk.com/software/taag/#p=display&f=Big%20Money-sw&t=Security-OAuth-1
+ * @author epr12
+ *
+ */
+@SpringBootApplication
+public class SecurityOauth1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityOauth1Application.class, args);
+	}
+
+}
